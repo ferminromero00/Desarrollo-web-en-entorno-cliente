c31488b5c6e93c76a2fb18b197e0f7336973aa78
ferminromero00 <ferminromero2005@gmail.com>
2024-09-30 17:44:54 +0200

    Add files via upload

---
+++ UNIDAD 1/RELACION 1.5/funciones.js
@@ -0,0 +1,17 @@
+function onlyUniques(...argumentos) {
+    let array = [];
+    array.push(argumentos)
+
+    return resultado = document.write(unique(array))
+}
+
+function squareAndSum(...numeros) {
+    let sum = 0;
+    for (let num of numeros) {
+        sum += num * num;
+    }
+    return sum;
+}
+
+
+
