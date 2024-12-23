3c13aa6841304389c8a4b53cae05689f43887043
Fermin <ferminromero2005@gmail.com>
2024-12-23 16:52:43 +0100

    Refactor index.js to streamline event initialization and update pasos.txt with additional product listing details; add new paginaPrincipal.js file

--- UNIDAD 6/PROYECTO TIENDA ONLINE/js/index.js
+++ UNIDAD 6/PROYECTO TIENDA ONLINE/js/index.js
@@ -1,14 +1,9 @@
 // Importa las funciones de manejo de eventos desde el archivo 'login.js'.
-// Estas funciones incluyen la lógica para iniciar sesión, registrar usuarios y borrar usuarios.
 import { eventLogin, eventRegistrar, eventBorrar } from './login.js';
 
 /**
- * Función principal que inicializa los eventos de la aplicación.
- * 
- * La función `main` se asegura de que los eventos correspondientes para login,
- * registro y borrado de usuarios estén correctamente configurados una vez que el
- * contenido del documento haya cargado por completo.
- */
+* Función principal que inicializa los eventos de la aplicación.
+*/
 const main = () => {
     // Configura el evento para el inicio de sesión.
     eventLogin();
@@ -17,5 +12,4 @@ const main = () => {
     // Configura el evento para borrar un usuario existente.
     eventBorrar();
 }
-
 document.addEventListener("DOMContentLoaded", main);
