1b169445a272459983886f57cd21775ede5a5794
Fermin <ferminromero2005@gmail.com>
2024-12-24 16:12:22 +0100

    Add category filtering and sorting functionality; update main page layout and enhance game listing display

--- UNIDAD 6/PROYECTO TIENDA ONLINE/js/index_Pagina_Principal.js
+++ UNIDAD 6/PROYECTO TIENDA ONLINE/js/index_Pagina_Principal.js
@@ -1,10 +1,11 @@
 // Importa las funciones de manejo de eventos desde el archivo 'login.js'.
-import { eventDibujar } from './paginaPrincipal.js';
+import { eventDibujar, eventCategorias } from './paginaPrincipal.js';
 
 /**
 * Función principal que inicializa los eventos de la aplicación.
 */
 const main = () => {
     eventDibujar()
+    eventCategorias()
 }
 document.addEventListener("DOMContentLoaded", main);
--- UNIDAD 6/PROYECTO TIENDA ONLINE/js/paginaPrincipal.js
+++ UNIDAD 6/PROYECTO TIENDA ONLINE/js/paginaPrincipal.js
@@ -11,7 +11,6 @@ function fetch1(url) {
     return fetch(url)
         .then(res => res.json())
         .then(json => {
-            console.log(json);
             return json;
         });
 }
@@ -19,41 +18,52 @@ function fetch1(url) {
 /* DIBUJAR JUEGOS Y SCROLL INFINITO*/
 
 export const eventDibujar = async () => {
-    let url = "http://localhost:3000/games"
-    dibujar(await fetch1(url));
+    let url = "http://localhost:3000/games";
+    dibujar(await fetch1(url), 1);
     cargarMas();
+    ordenar()
 }
 
 let paginaActual = 1;
 const juegosPorPagina = 8;
 let cargando = false;
+let categoriaSeleccionada = "";
+let ordenlistado = "desc"
 
-const dibujar = (url, pagina) => {
+const dibujar = (data, pagina) => {
+    data.sort((a, b) => {
+        if (ordenlistado === "asc") {
+            return a.cheapestPriceEver.price - b.cheapestPriceEver.price;
+        } else {
+            return b.cheapestPriceEver.price - a.cheapestPriceEver.price;
+        }
+    });
 
     let principio = (pagina - 1) * juegosPorPagina;
     let final = pagina * juegosPorPagina;
-    let objetos = url.slice(principio, final);
+    let objetos = data.slice(principio, final);
 
+    let lista = document.getElementById("lista_juegos");
 
-    let lista = document.getElementById("lista_juegos")
-    
     objetos.forEach(e => {
-        let div = document.createElement("div")
-        let h1 = document.createElement("h1")
-        let p = document.createElement("p")
-        let img = document.createElement("img")
-        let p2 = document.createElement("p")
+        let div = document.createElement("div");
+        let h1 = document.createElement("h1");
+        let p = document.createElement("p");
+        let img = document.createElement("img");
+        let p2 = document.createElement("p");
+        let br = document.createElement("br")
 
-        h1.textContent = e.info.title
-        p.textContent = "Precio: " + e.cheapestPriceEver.price
-        img.src = e.info.thumb
-        p2.textContent = "Categoria: " + e.info.category
+        h1.textContent = e.info.title;
+        p.textContent = "Precio: " + e.cheapestPriceEver.price + "€";
+        img.src = e.info.thumb;
+        p2.textContent = "Categoria: " + e.info.category;
 
-        lista.appendChild(div)
-        div.appendChild(h1)
-        div.appendChild(p)
-        div.appendChild(p2)
-        div.appendChild(img)
+        lista.appendChild(div);
+        div.appendChild(h1);
+        div.appendChild(p);
+        div.appendChild(p2);
+        div.appendChild(img);
+        lista.appendChild(br)
     });
 
     cargando = false;
@@ -65,32 +75,83 @@ const cargarMas = async () => {
     cargando = true;
     document.getElementById("cargando").style.display = "block";
 
-    let url = "http://localhost:3000/games";
+    let url = `http://localhost:3000/games?info.category=${categoriaSeleccionada}`;
     let data = await fetch1(url);
-    dibujar(data, paginaActual);
     paginaActual++;
+    dibujar(data, paginaActual);
 }
 
 window.addEventListener('scroll', () => {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        cargarMas();
+        cargarMas("http://localhost:3000/games");
     }
 });
 
 
+/* LISTAR POR CATEGORIAS */
 
+export const eventCategorias = async () => {
+    let url = "http://localhost:3000/games"
+    let categorias = listarCategorias(await fetch1(url));
+    insertarCategorias(categorias);
+    filtrar();
+}
 
+const listarCategorias = (url) => {
+    let categorias = []
 
+    url.forEach(e => {
 
+        if (!categorias.includes(e.info.category)) {
+            categorias.push(e.info.category)
+        }
+    })
+    return categorias;
+}
 
+const insertarCategorias = (lista) => {
+    let lista_html = document.getElementById("lista_categorias")
+    lista.forEach(e => {
+        let option = document.createElement("option")
+        option.textContent = e
+        option.id = "seleccion"
+        lista_html.appendChild(option)
+    })
+}
 
+const filtrar = () => {
+    let lista_html = document.getElementById("lista_categorias");
 
+    lista_html.addEventListener("change", async (event) => {
+        categoriaSeleccionada = event.target.value;
+        paginaActual = 1;
 
+        document.getElementById("lista_juegos").innerHTML = "";
+        let url = `http://localhost:3000/games?info.category=${categoriaSeleccionada}`;
+        dibujar(await fetch1(url), paginaActual);
 
+        console.log(url);
 
+    });
 
+}
 
+/* CAMBIAR ASCENDENTE O DESCENDENTE */
 
+const ordenar = () => {
+    let ordenar_btn = document.getElementById("ordenar_btn");
+
+    ordenar_btn.addEventListener("click", async () => {
+        ordenlistado = ordenlistado === "asc" ? "desc" : "asc";
+        ordenar_btn.textContent = `Ordenar: ${ordenlistado === "asc" ? "Ascendente" : "Descendente"}`;
+        paginaActual = 1;
+
+        document.getElementById("lista_juegos").innerHTML = "";
+        let url = `http://localhost:3000/games?info.category=${categoriaSeleccionada}&_sort=cheapestPriceEver.price&_order=${ordenlistado}`;
+        let data = await fetch1(url);
+        dibujar(data, paginaActual);
+    });
+}
 
 
 
