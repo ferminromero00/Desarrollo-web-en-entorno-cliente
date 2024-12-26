9a6cbf362f258e3d9ec4b1b08c88847243f2a177
Fermin <ferminromero2005@gmail.com>
2024-12-26 14:01:44 +0100

    Refactor product detail handling; move dibujarProductoSeleccionado function to paginaPrincipal.js and update event initialization in index_Pagina_Principal.js

--- UNIDAD 6/PROYECTO TIENDA ONLINE/js/index_Pagina_Principal.js
+++ UNIDAD 6/PROYECTO TIENDA ONLINE/js/index_Pagina_Principal.js
@@ -1,13 +1,19 @@
 // Importa las funciones de manejo de eventos desde el archivo 'login.js'.
-import { eventDibujar, eventCategorias, eventCarrito, eventVerMasInformacion } from './paginaPrincipal.js';
+import { eventDibujar, eventCategorias, eventCarrito, eventVerMasInformacion, dibujarProductoSeleccionado } from './paginaPrincipal.js';
 
 /**
 * Función principal que inicializa los eventos de la aplicación.
 */
 const main = () => {
-    eventDibujar()
-    eventCategorias()
-    eventCarrito()
-    eventVerMasInformacion()
+    let path = window.location.pathname;
+
+    if (path.includes('paginaPrincipal.html')) {
+        eventDibujar()
+        eventCategorias()
+        eventCarrito()
+        eventVerMasInformacion()
+    } else if (path.includes('informacionProducto.html')) {
+        dibujarProductoSeleccionado()
+    }
 }
 document.addEventListener("DOMContentLoaded", main);
--- UNIDAD 6/PROYECTO TIENDA ONLINE/js/paginaPrincipal.js
+++ UNIDAD 6/PROYECTO TIENDA ONLINE/js/paginaPrincipal.js
@@ -285,5 +285,31 @@ export const eventVerMasInformacion = (datos) => {
     }
 }
 
+export const dibujarProductoSeleccionado = () => {
+    let infoProduct = JSON.parse(localStorage.getItem("productoseleccionado"));
 
+    let lista = document.getElementById("lista")
+    console.log(infoProduct);
+
+    let div = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+    let img = document.createElement("img");
+    let p2 = document.createElement("p");
+    let button = document.createElement("button")
+
+    h1.textContent = infoProduct.info.title;
+    p.textContent = "Precio: " + infoProduct.cheapestPriceEver.price + "€";
+    img.src = infoProduct.info.thumb;
+    p2.textContent = "Categoria: " + infoProduct.info.category;
+    button.textContent = "Añadir"
+    button.addEventListener("click", () => { eventCarrito(infoProduct); });
+
+    lista.appendChild(div);
+    div.appendChild(h1);
+    div.appendChild(p);
+    div.appendChild(p2);
+    div.appendChild(img);
+    lista.appendChild(button)
+}
 
