59bbd47660228083d3d46c75f15c936390a75b85
Fermin <ferminromero2005@gmail.com>
2025-01-10 09:05:23 +0100

    Add Parrafo component and integrate it into MyApp for displaying articles

---
+++ UNIDAD 7/PRIMER_PROYECTO_REACT/Ejemplo1/components/Parrafo.jsx
@@ -0,0 +1,5 @@
+export default function Parrafo({ children, codigo }) {
+    return (
+        <p> {codigo}: {children} </p>
+    );
+}
---
+++ UNIDAD 7/PRIMER_PROYECTO_REACT/Ejemplo1/src/MyApp.jsx
@@ -0,0 +1,11 @@
+import Parrafo from "../components/Parrafo";
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Hola mundo</h1>
+            <Parrafo codigo='1'>Articulo 1</Parrafo>
+            <Parrafo codigo='2'>Articulo 2</Parrafo>
+        </div>
+    );
+}
