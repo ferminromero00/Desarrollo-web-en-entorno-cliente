f4f0bd70f4fe627eba53aa41afb5bc9785e92f0b
Fermin <ferminromero2005@gmail.com>
2025-01-10 09:34:41 +0100

    Refactor Cabecera component to use CSS modules for styling; add Cabecera.module.css with updated styles

--- UNIDAD 7/PRIMER_PROYECTO_REACT/Ejemplo1/components/Cabecera.jsx
+++ UNIDAD 7/PRIMER_PROYECTO_REACT/Ejemplo1/components/Cabecera.jsx
@@ -1,5 +1,7 @@
+import styles from "./Cabecera.module.css";
+
 export default function Cabecera({ children }) {
     return (
-        <h1 style={{color: "red"}}> {children} </h1>
+        <h1 className={styles}> {children} </h1>
     );
 }
