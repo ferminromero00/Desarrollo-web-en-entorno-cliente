a533d8657d718fbe118bf09a734a3ced77244750
Fermin <ferminromero2005@gmail.com>
2025-01-23 17:30:56 +0100

    Refactor main functions in index_login and index_principal for improved conciseness

--- Examen/RepasoFetch/js/index_login.js
+++ Examen/RepasoFetch/js/index_login.js
@@ -1,7 +1,5 @@
 import { EventValidar } from "./components/validar.js";
 
-const main = () => {
-    EventValidar()
-}
+const main = () => { EventValidar() }
 
 document.addEventListener("DOMContentLoaded", main);
--- Examen/RepasoFetch/js/index_principal.js
+++ Examen/RepasoFetch/js/index_principal.js
@@ -2,10 +2,6 @@ import { EventDibujar } from "./components/dibujar.js";
 import { EventFiltrar } from "./components/filtrar.js";
 import { EventCategorias } from "./components/Generos.js";
 
-const main = () => {
-    EventDibujar()
-    EventFiltrar()
-    EventCategorias()
-}
+const main = () => { EventDibujar(); EventFiltrar(); EventCategorias() }
 
 document.addEventListener("DOMContentLoaded", main);
