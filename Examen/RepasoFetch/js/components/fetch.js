4921a63a9766bcb196e90d5d1a56e06bb29a1321
Fermin <ferminromero2005@gmail.com>
2025-01-23 17:39:17 +0100

    Refactor fetchUsuarios and fetchProductos functions for improved conciseness and readability

--- Examen/RepasoFetch/js/components/fetch.js
+++ Examen/RepasoFetch/js/components/fetch.js
@@ -1,12 +1,10 @@
-export const fetchUsuarios = () => {
-    return fetch('https://fakestoreapi.com/users').then(res => res.json())
-}
+export const fetchUsuarios = () => { return fetch('https://fakestoreapi.com/users').then(res => res.json()) }
 
 export function fetchProductos() {
     return new Promise((resolve) => {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "https://fakestoreapi.com/products", true);
-        xhr.onload = function () {const productos = JSON.parse(xhr.responseText); resolve(productos);};
+        xhr.onload = function () { const productos = JSON.parse(xhr.responseText); resolve(productos); };
         xhr.send();
     });
 }
