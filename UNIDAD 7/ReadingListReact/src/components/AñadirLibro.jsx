31e5e9d548adcd7e70edaaaed00c2c4e004939aa
Fermin <ferminromero2005@gmail.com>
2025-01-27 19:56:17 +0100

    Update AñadirLibro component to enhance label clarity for title, author, and genre

--- UNIDAD 7/ReadingListReact/src/components/AñadirLibro.jsx
+++ UNIDAD 7/ReadingListReact/src/components/AñadirLibro.jsx
@@ -4,9 +4,9 @@ export default function AñadirLibro({ title, author, genre }) {
   return (
     <div className="card mb-3">
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{author}</h6>
-        <p className="card-text">{genre}</p>
+        <h5 className="card-title">Titulo: {title}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Autor: {author}</h6>
+        <p className="card-text">Genero: {genre}</p>
       </div>
     </div>
   );
