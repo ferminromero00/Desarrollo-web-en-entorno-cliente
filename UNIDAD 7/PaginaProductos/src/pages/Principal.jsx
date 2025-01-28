47edbc9d507fd7b9aa97201961a9ab7f43529c35
Fermin <ferminromero2005@gmail.com>
2025-01-28 13:44:14 +0100

    Update product page title and refactor Fetch utility for improved data handling

--- UNIDAD 7/PaginaProductos/src/pages/Principal.jsx
+++ UNIDAD 7/PaginaProductos/src/pages/Principal.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Navegacion from '../components/Navegacion'
+import React from 'react';
+import Navegacion from '../components/Navegacion';
+import { Fetch } from '../utils/Fetch';
+import { useEffect } from 'react';
 
 export default function Principal() {
+  useEffect(() => { Fetch('https://fakestoreapi.com/products'); }, []);
+
   return (
     <>
-        <Navegacion></Navegacion>
+      <Navegacion />
     </>
-  )
+  );
 }
--- UNIDAD 7/PaginaProductos/src/utils/Fetch.jsx
+++ UNIDAD 7/PaginaProductos/src/utils/Fetch.jsx
@@ -1,27 +1,5 @@
-import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react'
-
-export default function Fetch() {
-
-    const [data, setData] = useState([]);
-
-    useEffect(() => { fetch('https://fakestoreapi.com/products').then(res => res.json()).then(json => { setData(json); }) }, []);
-
-    return (
-        <>
-            {data.map((item) => {
-                return (
-                    <>
-                        <div key={item.id}>
-                            <h1>{item.title}</h1>
-                            <img src={item.image} width={130} />
-                            <p>{item.description}</p>
-                            <p>Price: {item.price} € </p>
-                        </div>
-                    </>
-                )
-            })}
-        </>
-    )
-}
+export const Fetch = (URL) => {
+    return fetch(URL)
+        .then(res => res.json())
+        .then(json => console.log(json))
+};
