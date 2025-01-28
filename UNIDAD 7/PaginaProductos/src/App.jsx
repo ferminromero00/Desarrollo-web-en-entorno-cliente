c87f7598c7502976dbf8f09b66be51c8b1ac60f7
Fermin <ferminromero2005@gmail.com>
2025-01-28 13:11:59 +0100

    Add product page structure and components; integrate Bootstrap for styling and implement product fetching

--- UNIDAD 7/PaginaProductos/src/App.jsx
+++ UNIDAD 7/PaginaProductos/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import Principal from './pages/Principal'
 
 function App() {
-
   return (
     <>
-
+      <Principal></Principal>
     </>
   )
 }
---
+++ UNIDAD 7/PaginaProductos/src/pages/Principal.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navegacion from '../components/Navegacion'
+
+export default function Principal() {
+  return (
+    <>
+        <Navegacion></Navegacion>
+    </>
+  )
+}
