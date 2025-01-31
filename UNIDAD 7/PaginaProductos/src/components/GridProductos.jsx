8e0702f1d06e8418f737c3e2dd10b45b0bc327a0
Fermin <ferminromero2005@gmail.com>
2025-01-31 10:09:56 +0100

    Refactor GridProductos component and implement Carrusel for dynamic product display with navigation

--- UNIDAD 7/PaginaProductos/src/components/GridProductos.jsx
+++ UNIDAD 7/PaginaProductos/src/components/GridProductos.jsx
@@ -14,7 +14,7 @@ export default function GridProductos() {
         <div className="row">
           {products.map(e => (
             <div className="col-md-6 mb-4 mt-3" key={e.id}>
-              <Link to={`/detalle/${e.id}`} className="nav-link active">
+              <Link to={`/detalle/${e.id}`}>
                 <div className="card h-100 border border-5">
                   <div className="d-flex justify-content-center align-items-center">
                     <img src={e.image} className="card-img-top" alt={e.title} style={{ height: '300px', width: '300px', marginTop: '1rem' }} />
