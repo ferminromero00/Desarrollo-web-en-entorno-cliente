3ae0c4044df85bcc15f35b7098e2db772eb099c2
Fermin <ferminromero2005@gmail.com>
2025-02-17 11:16:29 +0100

    Actualizar texto en App.js para reflejar la pantalla de inicio de sesión y agregar descripción sobre conocimientos avanzados de React Native

--- UNIDAD 8/ReactNative1/App.js
+++ UNIDAD 8/ReactNative1/App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Login</Text>
+      <Text>Conocimientos avanzandos de React Native</Text>
+      
     </View>
   );
 }
@@ -18,3 +19,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
