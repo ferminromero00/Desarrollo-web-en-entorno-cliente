48c38bc90219d1283600fd1c4eac94e9890b6284
Fermin <ferminromero2005@gmail.com>
2025-02-17 12:02:32 +0100

    Agregar funcionalidad de inicio de sesión en App.js; incluir campos de entrada para usuario y contraseña, y un botón de acceso

--- UNIDAD 8/ReactNative1/App.js
+++ UNIDAD 8/ReactNative1/App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [login, setLogin] = useState("");
+  const [pass, setPass] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>Login</Text>
-      <Text>Conocimientos avanzandos de React Native</Text>
-      
+      <TextInput style={styles2.input} value={setLogin}></TextInput>
+      <Text>Pass</Text>
+      <TextInput style={styles2.input} value={setPass}></TextInput>
+
+      <Button
+        title="Acceder"
+        onPress={() => {
+          alert("Boton presionado");
+        }}
+      ></Button>
     </View>
   );
 }
@@ -14,10 +25,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
-
+const styles2 = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+  },
+});
