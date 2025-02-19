4efcd107ce68e474baba9d0180bcb26406cb7232
Fermin <ferminromero2005@gmail.com>
2025-02-19 17:13:06 +0100

    das

--- RepasoReact/RepasoExamen2/src/App.jsx
+++ RepasoReact/RepasoExamen2/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Principal from "../../RepasoExamen2/src/pages/Principal";
+
 function App() {
 
   return (
     <>
-     
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Principal />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
